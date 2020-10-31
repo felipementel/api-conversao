7cb3b52e9530af0314009246e27d291417f1d438
Felipe Augusto <felipementel@hotmail.com>
2020-10-31 15:15:53 -0300

    v1

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const os = require('os')
+const app = express();
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); 
+
+app.get('/fahrenheit/:valor/celsius', (req, res) => {
+
+    let valor = req.params.valor;
+    let celsius = (valor - 32) * 5 / 9;
+    res.json({ "celsius": celsius, "maquina": os.hostname() });
+});
+
+app.get('/celsius/:valor/fahrenheit', (req, res) => {
+
+    let valor = req.params.valor;
+    let fahrenheit = (valor * 9 / 5) + 32;
+    res.json({ "fahrenheit": fahrenheit, "maquina": os.hostname() });
+});
+
+app.listen(8090, () => {
+    console.log("Servidor rodando na porta 8090");
+});
